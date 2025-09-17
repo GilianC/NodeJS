2718431a15a88cdaeb863e433937d87933a9c34f
GilianC <163392501+GilianC@users.noreply.github.com>
2025-09-17 10:29:42 +0200

    migration1

---
+++ prisma/migrations/20250917082851_update1/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "pseudo" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_pseudo_key" ON "public"."User"("pseudo");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
